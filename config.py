435752847369023cdb9e58850ee4ebf52cf60fa9
Martin Ruiz <martin.ruiz.mares@gmail.com>
2021-02-04 18:31:31 -0600

    first commit

---
+++ config.py
@@ -0,0 +1,4 @@
+ckey = 'jZdVFHEMopbJj3jCaQhgBDIbe'
+csecret = 'p4jkCMUc1uI0G8RTi6GrSXJjPawaBZ2Wd9rSk5CHKCNPJbCsL6'
+atoken = '737400695620440064-vkRKOdlrizjcpRoC4XsU4fhGTRoYetA'
+asecret = '5DxaEXGHXQVQtxJeQuOcbagSsYQh6Vz6XPenm86HZECkR'
